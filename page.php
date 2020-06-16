8a2c13a42ce852596789f7dd34dbc63ed3bc9abe
Johannes Maas <y0hy0h@gmx.net>
2020-06-16 20:56:55 +0200

    Display page titles when not on frontpage

--- page.php
+++ page.php
@@ -9,10 +9,15 @@
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			
 			<?php if (has_post_thumbnail()) the_post_thumbnail(); ?>
+
+                <?php if (!is_front_page()) : ?>
+                <h1><?php the_title(); ?></h1>
+                <?php endif; ?>
+
 				<?php the_content(); ?>
 				
 				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
-								
+
 		</div>
 		
 		<?php endwhile; ?>
