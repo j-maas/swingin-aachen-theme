971d6ecbc56d2c453491b337e57ec4327b53edfa
Johannes Maas <y0hy0h@gmx.net>
2020-06-17 18:53:52 +0200

    Add seal

--- footer.php
+++ footer.php
@@ -1,5 +1,8 @@
-		<footer>
+        <img src="<?php echo get_template_directory_uri()?>/images/seal.svg" class="seal"/>
+        <footer>
+            <nav>
             <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
+            </nav>
 		</footer>
 		
 	</div>
