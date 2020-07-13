370a4280d09ae55e2df213110295dc3afd22f710
Johannes Maas <y0hy0h@gmx.net>
2020-07-13 23:35:10 +0200

    Show title heading on frontpage

--- page.php
+++ page.php
@@ -9,10 +9,7 @@
 		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 			
 			<?php if (has_post_thumbnail()) the_post_thumbnail(); ?>
-
-                <?php if (!is_front_page()) : ?>
                 <h1><?php the_title(); ?></h1>
-                <?php endif; ?>
 
 				<?php the_content(); ?>
 				
