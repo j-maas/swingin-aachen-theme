da69ab6c7a82597f95e0c02c93365ced5a2d4d8f
Johannes Maas <y0hy0h@gmx.net>
2020-07-14 01:38:17 +0200

    Lazy-load seal

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-        <img src="<?php echo get_template_directory_uri()?>/images/seal.svg" widht="256" height="256" class="seal" alt="" role="presentation"/>
+        <img src="<?php echo get_template_directory_uri()?>/images/seal.svg" width="256" height="256" class="seal" alt="" role="presentation" loading="lazy"/>
         <footer>
             <nav>
             <?php wp_nav_menu( array( 'theme_location' => 'footer-menu' ) ); ?>
