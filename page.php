7bf76df3289bc3996b02405f56e3cb713f1cfc48
Johannes Maas <github@j-maas.de>
2022-02-19 15:48:20 +0100

    Add backlink for subpages

--- page.php
+++ page.php
@@ -1,30 +1,36 @@
 <?php get_header(); ?>
-	
-	<main>
-	
 	<?php if (have_posts()) : ?>
 		
 		<?php while (have_posts()) : the_post(); ?>
-		
-		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+		<?php
+			$parent = get_post_parent();
+			if ($parent) { ?>
+			<nav class="backlink">
+				<a href="<?php echo get_permalink($parent->ID); ?>">Zurück zu "<?php echo $parent->post_title; ?>"</a>
+			</nav>
+		<?php } ?>
+		<main>
 			
-			<?php if (has_post_thumbnail()) the_post_thumbnail(); ?>
-                <h1><?php the_title(); ?></h1>
-
-				<?php the_content(); ?>
+			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				
-				<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+				<?php if (has_post_thumbnail()) the_post_thumbnail(); ?>
+					<h1><?php the_title(); ?></h1>
 
-		</div>
+					<?php the_content(); ?>
+					
+					<?php wp_link_pages(array('before' => 'Pages: ', 'next_or_number' => 'number')); ?>
+
+			</div>
+		</main>
 		
 		<?php endwhile; ?>
+	
 
 	<?php else : ?>
-		
-		<?php get_template_part('inc/gone'); ?>
-		
+		<main>
+			<?php get_template_part('inc/gone'); ?>
+		</main>
 	<?php endif; ?>
-	
-	</main>
 
 <?php get_footer(); ?>
